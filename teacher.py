f4ae073724262d653cbd6cc63e3db4d6c2deadcd
riyakamat-0605 <riyakamat2006@gmail.com>
2025-10-30 15:36:56 +0530

    save to teacher

---
+++ teacher.py
@@ -0,0 +1,9 @@
+Name = (input("Enter Name of the faculty: "))
+Subject = (input("Enter The Course Name: "))
+Course_code = (input("Enter the course code: "))
+
+#printing teacher details
+print("Teachers Details: ")
+print(f"Faculty Name: {Name} ")
+print(f"Subject: {Subject}")
+print(f"Course Code: {Course_code}")
